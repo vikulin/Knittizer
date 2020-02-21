513703676f4e93785313624b70b742b8afbd3373
Vadym <vadym.vikulin@gmail.com>
2020-02-21 21:05:55 +0200

    1. moved html to resource file

--- app/src/main/java/org/vikulin/knittizer/HelpActivity.java
+++ app/src/main/java/org/vikulin/knittizer/HelpActivity.java
@@ -6,19 +6,12 @@ import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.widget.LinearLayout;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 
 public class HelpActivity extends AlertActivity {
 
-    private final String sampleHtml =
-            "<?xml version=\"1.0\" encoding=\"UTF-8\" ?><html>\n" +
-            "<font color=\"#000000\"><font size=\"4\" style=\"font-size: 16pt;\"><b><p style=\"text-indent: 20px;\">Подготовка образца</p></b></font></font></p>\n" +
-            "<p style=\"margin-left: 0.12in; margin-bottom: 0.14in; line-height: 120%; text-indent: 20px;\">\n" +
-            "<font size=\"2\" style=\"font-size: 14pt\">Для расчёта петельной пробы необходимо связать образец с определённым количеством петель и рядов.\n" +
-            "Затем тщательно отпарить образец, и, дав ему остыть, измерить его линейкой по вертикали и горизонтали.\n" +
-            "Полученные цифры измерений ввести в соответствующие поля программы вместе с цифрами петель и рядов: А и В.\n" +
-            "Программа автоматически расчитает плотность образца, то есть количество петель и рядов в 1 см.</font></p>\n" +
-            "</html>";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,7 +41,7 @@ public class HelpActivity extends AlertActivity {
                     setTitle(getResources().getString(R.string.help)+": "+getResources().getString(R.string.sample_calculate_menu).toLowerCase());
                     String base64 = null;
                     try {
-                        base64 = Base64.encodeToString(sampleHtml.getBytes("UTF-8"), Base64.DEFAULT);
+                        base64 = Base64.encodeToString(readResource(R.raw.sample_html), Base64.DEFAULT);
                         help.loadData(base64, "text/html; charset=utf-8", "base64");
                     } catch (UnsupportedEncodingException e) {
                         e.printStackTrace();
@@ -63,5 +56,12 @@ public class HelpActivity extends AlertActivity {
         } else {
             finish();
         }
+    }
+
+    private byte[] readResource(int id) throws IOException {
+        InputStream is = getResources().openRawResource(id);
+        byte[] b = new byte[is.available()];
+        is.read(b);
+        return b;
     }
 }
